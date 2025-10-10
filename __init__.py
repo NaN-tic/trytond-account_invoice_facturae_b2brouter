d8b09bfe17cde1878c4a59a8bdb5f92242bb53f8
Bernat Brunet <bernat@nan-tic.com>
2025-10-10 15:17:29 +0200

    Update the way that update the send invoice status. Not us interval of days, download all invoices states that are in unfinished states.
    Add a button to allow update invoices individually and not to way for
    the cron process.
    
    Add button to allow resend the invoice if it's needed. This will remove
    the invoice send before and create a new one.
    
    Add script to get the possible invoice states.

--- __init__.py
+++ __init__.py
@@ -2,14 +2,11 @@
 # The COPYRIGHT file at the top level of this repository contains
 # the full copyright notices and license terms.
 from trytond.pool import Pool
-from . import configuration
 from . import invoice
 from . import cron
 
 def register():
     Pool.register(
-        configuration.Configuration,
-        configuration.ConfigurationFacturae,
         invoice.Invoice,
         invoice.GenerateFacturaeStart,
         cron.Cron,
